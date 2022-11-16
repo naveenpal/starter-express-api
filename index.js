993fe0e084b8ea5247fdbf0f6111e372283c0076
naveen <naveenpal.singh@yahoo.com>
2022-11-16 07:35:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const {mongodb } = require('mongodb')
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('HI Naveen pal singh')
